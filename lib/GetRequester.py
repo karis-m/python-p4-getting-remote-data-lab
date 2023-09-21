317d47b741c522091019b38af7b442a9f11d46b9
karis-m <moses.kamau@student.moringaschool.com>
2023-09-21 14:25:05 +0300

    Done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -17,12 +17,11 @@ class GetRequester:
         people = json.loads(self.get_response_body())
         
         for person in people:
-            peoples_list.append(person["name"])
+            peoples_list.append(person)
+            print(peoples_list)
         return peoples_list
 
 get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
-people_Details = get_requester.load_json()
+get_requester.load_json()
 
-for each_person in set(people_Details):
-    print(each_person)
 # get_requester.get_response_body()
